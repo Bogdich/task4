0d716690416a47a00aa0899d008b57321d256a66
eneguE <bogdevich96@hotmail.com>
2017-05-07 15:45:29 +0300

    type

---
+++ src/main/java/com/bogdevich/task4/entity/type/ComponentType.java
@@ -0,0 +1,9 @@
+package com.bogdevich.task4.entity.type;
+
+/**
+ * Created by Adrienne on 24.04.17.
+ */
+public enum ComponentType {
+    TEXT, PARAGRAPH, SENTENCE, LEXEME, WORD, SYMBOL, EXPRESSION
+}
+
